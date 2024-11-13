897139d7dce008fe041f02f3c04119e71a9bb199
Henry Rogers <henry_rogers@trimble.com>
2024-11-14 11:27:14 +1300

    Creation, encode and decode of PSK and ASK complete

--- Driver.py
+++ Driver.py
@@ -2,17 +2,17 @@ import Utility_functions
 
 
 def main():
-    working_file_wave = "encoded-ASK"
+    working_file_wave = "encoded-PSK"
     to_encode =Utility_functions.ascii_binary_translator("hello world")
-    mode = "ASK"
+    mode = "PSK"
     frequency = 4
     end_time = 1
     sample_rate = 50
     #to_decode = Utility_functions.load_wave(working_file_wave)
     to_decode = Utility_functions.wave_creator(end_time, sample_rate, frequency, to_encode, mode)
     Utility_functions.show_wave(to_decode)
-    decoded = Utility_functions.decode_amplitude_shift_keying(to_decode, frequency, sample_rate)
+    decoded = Utility_functions.decode_phase_shift_keying(to_decode, frequency, sample_rate)
     print(decoded)
-    to_decode = Utility_functions.ascii_binary_translator(decoded)
-    print(to_decode)
+    ASCII = Utility_functions.ascii_binary_translator(decoded)
+    print(ASCII)
 main()
--- Utility_functions.py
+++ Utility_functions.py
@@ -33,8 +33,11 @@ def ascii_binary_translator(translate_me):
 
 def single_bit_generator(frequency, time, amplitude, theta, mode):
     if mode == "ASK":
-        amplitude += 1
+        amplitude += 1  # double the amplitude
         encoded_bit = amplitude * np.sin(2 * np.pi * frequency * time + theta)
+    elif mode == "PSK":
+        encoding = int(theta) * 3  # 3 seems to move it 180 out of phase
+        encoded_bit = amplitude * np.sin(2 * np.pi * frequency * time + encoding)
     return encoded_bit
 
 
@@ -50,8 +53,12 @@ def wave_creator(end_time, sample_rate, frequency, bits_to_encode, mode):
     time = np.arange(start_time, end_time, 1 / sample_rate)
     total_wave = []
     bits_encoded = 0
+    amplitude = 1
     while bits_encoded < len(bits_to_encode):
-        amplitude = bits_to_encode[bits_encoded]
+        if mode == "ASK":
+            amplitude = bits_to_encode[bits_encoded]
+        elif mode == "PSK":
+            theta = bits_to_encode[bits_encoded]
         sinewave = single_bit_generator(frequency, time, int(amplitude), theta, mode)
         for i in sinewave:
             total_wave.append(i)
@@ -86,13 +93,33 @@ def load_wave(file):
     return arr
 
 
-def decode_phase_shift_keying():
+def decode_phase_shift_keying(to_decode, frequency, sample_rate):
     """
     A phase shift key takes in our received wave, and checks it against our reference wave.
     This reference wave is a known value published by the operator of the transmitter.
     if we are in phase, that is a 1, out of phase by 180 degrees makes it a 0
     """
-    return
+    buff_start = 0
+    buff_end = (sample_rate - 1)
+    decoded_bits = ""
+
+    # I am going to check to see if the section trend is positive or negative e.g. phase of 0 or 3
+    while buff_end < len(to_decode):
+        buff_section = to_decode[buff_start:buff_end]
+        ave = 1
+        buff_trend = 0
+        while ave <= frequency:
+            peak = float(buff_section[ave])
+            buff_trend += peak
+            print(buff_trend)
+            ave += 1
+        if buff_trend > 0:
+            decoded_bits = "{}0".format(decoded_bits)
+        else:
+            decoded_bits = "{}1".format(decoded_bits)
+        buff_start = buff_end
+        buff_end += sample_rate
+    return decoded_bits
 
 
 def decode_amplitude_shift_keying(to_decode, frequency, sample_rate):
