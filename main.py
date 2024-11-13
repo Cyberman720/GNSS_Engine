91927b68f772849a8e3278e24eedb42117f20627
Henry Rogers <henry_rogers@trimble.com>
2024-11-13 13:27:09 +1300

    Set up the base funcntions

---
+++ main.py
@@ -0,0 +1,37 @@
+"""
+This is a basic python script that takes in a sine wave, and converts it to bits
+I will be implementing the three major types of shift keying to do this.
+"""
+def wave_creator():
+    """
+    a utility function that takes our binary and puts it into a sine wave.
+    """
+    return
+
+def load_wave():
+    """load an existing wave"""
+    return
+
+
+def Phase_shift_keying():
+    """
+    A phase shift key takes in our received wave, and checks it against our reference wave.
+    This reference wave is a known value published by the operator of the transmitter.
+    if we are in phase, that is a 1, out of phase by 180 degrees makes it a 0
+    """
+    return
+
+def Amplitude_shift_keying():
+    """
+    amplitude shift keying takes in the wave amplitude on the Y axies.
+    If the aplitude is doubled, that makes it a 1, and if the amplitude is halved, it is a 0
+    """
+    return
+
+def Frequency_shift_keying():
+    """
+    Frequency shift keying relys on the wave doubling or halving.
+    doubling the frequency of our reference wave would be a 0 in our case, with satandard frequency being 1
+    """
+
+    return
