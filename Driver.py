149bab5fe74150d29d6a7a77d0e0e3020952f3e9
Henry Rogers <henry_rogers@trimble.com>
2024-11-18 11:23:57 +1300

    Created new Gold_code creator file, so I can now encode the GPS gold codes.

--- Driver.py
+++ Driver.py
@@ -1,10 +1,11 @@
+import Gold_code_creator
 import Utility_functions
 
 
 def main():
-    to_encode =Utility_functions.ascii_binary_translator("hello world")
+    to_encode = Gold_code_creator.Create_gold_code(24)
+    #to_encode =Utility_functions.ascii_binary_translator("hello world")
     mode = "PSK"
-    waves_per_bit = 1
     measured_frequency = 1545420000
     bits_per_second = 50
     frequency_per_bit = measured_frequency / bits_per_second
@@ -15,6 +16,6 @@ def main():
     to_decode = Utility_functions.wave_creator(total_samples, frequency_per_bit, to_encode, mode)
     decoded = Utility_functions.decode_phase_shift_keying(to_decode, frequency_per_bit, total_samples)
     print(decoded)
-    ASCII = Utility_functions.ascii_binary_translator(decoded)
-    print(ASCII)
+    #ASCII = Utility_functions.ascii_binary_translator(decoded)
+    #print(ASCII)
 main()
--- Gold_code_creator.py
+++ Gold_code_creator.py
@@ -1,6 +1,3 @@
-import matplotlib.pyplot as plt
-
-
 def shift(register, feedback, output):
     """
     mimics the feedback loop for the G1 and G2 portions for a GPS goldcode.
@@ -26,7 +23,7 @@ def shift(register, feedback, output):
     return out
 
 
-def prn(sv):
+def prn(sv, SV_table):
     """
     takes in sv number and will generate the G1+G2 ca code for it.
     """
@@ -39,51 +36,61 @@ def prn(sv):
     # create sequence
     for i in range(1023):
         g1 = shift(G1, [3, 10], [10])
-        g2 = shift(G2, [2, 3, 6, 8, 9, 10], SV[sv])
+        g2 = shift(G2, [2, 3, 6, 8, 9, 10], SV_table[sv])
         # modulo 2 add and append to the code
         ca.append((g1 + g2) % 2)
     return ca
 
 
-SV = {
-   1: [2, 6],
-   2: [3, 7],
-   3: [4, 8],
-   4: [5, 9],
-   5: [1, 9],
-   6: [2, 10],
-   7: [1, 8],
-   8: [2, 9],
-   9: [3, 10],
-   10: [2, 3],
-   11: [3, 4],
-   12: [5, 6],
-   13: [6, 7],
-   14: [7, 8],
-   15: [8, 9],
-   16: [9, 10],
-   17: [1, 4],
-   18: [2, 5],
-   19: [3, 6],
-   20: [4, 7],
-   21: [5, 8],
-   22: [6, 9],
-   23: [1, 3],
-   24: [4, 6],
-   25: [5, 7],
-   26: [6, 8],
-   27: [7, 9],
-   28: [8, 10],
-   29: [1, 6],
-   30: [2, 7],
-   31: [3, 8],
-   32: [4, 9],
-}
-f_prn = 10.23e6 / 10  # chipping frequency (1023b/s)
+def stringify(to_encode):
+    """simple helper that turns list of binary values into string to send other places"""
+    binary_string = ""
+    for i in to_encode:
+        binary_string = "{}{}".format(binary_string, i)
+    return binary_string
 
-# find ca code for sat 24, and make 0 into -1 to use in our wave form functions
-sat_24 = [0 if x == 0 else x for x in prn(24)]
-for i in sat_24:
-    sat_prn = sat_24[int(i * f_prn) % 1023]
-sat = plt.plot(sat_24)
-plt.show()
+
+def Create_gold_code(SV):
+    """ the driver function that takes in the sat you are looking for and outputs the phony gold code for it"""
+    SV_table = {
+        1: [2, 6],
+        2: [3, 7],
+        3: [4, 8],
+        4: [5, 9],
+        5: [1, 9],
+        6: [2, 10],
+        7: [1, 8],
+        8: [2, 9],
+        9: [3, 10],
+        10: [2, 3],
+        11: [3, 4],
+        12: [5, 6],
+        13: [6, 7],
+        14: [7, 8],
+        15: [8, 9],
+        16: [9, 10],
+        17: [1, 4],
+        18: [2, 5],
+        19: [3, 6],
+        20: [4, 7],
+        21: [5, 8],
+        22: [6, 9],
+        23: [1, 3],
+        24: [4, 6],
+        25: [5, 7],
+        26: [6, 8],
+        27: [7, 9],
+        28: [8, 10],
+        29: [1, 6],
+        30: [2, 7],
+        31: [3, 8],
+        32: [4, 9],
+    }
+    f_prn = 10.23e6 / 10  # chipping frequency (1023b/s)
+
+    # find ca code for sat 24, and make 0 into -1 to use in our wave form functions
+    target_sat_CA = [0 if x == 0 else x for x in prn(SV, SV_table)]
+    for i in target_sat_CA:
+        sat_prn = target_sat_CA[int(i * f_prn) % 1023]
+    binary_string = stringify(sat_prn)
+    return binary_string
